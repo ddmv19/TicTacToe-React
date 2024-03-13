5f34185b65dae6ea8e430c40d69d9c9e9b1cf007
ddmv19 <moronvillanuevadiego@outlook.com>
2024-03-13 00:56:43 -0500

    Remove unnecesary code

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -5,15 +5,13 @@ export const winnerPlays = [
   '036',
   '147',
   '258',
-  '047',
+  '048',
   '246',
 ]
 
 export const checkWinner = (board = []) => {
   const elementsUndefined = board.filter((element) => !element).length
-  console.log({ elementsUndefined })
   if (elementsUndefined <= 4) {
-    console.log('Posible ganador')
     for (let i = 0; i < winnerPlays.length; i++) {
       const first = parseInt(winnerPlays[i].charAt(0))
       const second = parseInt(winnerPlays[i].charAt(1))
@@ -23,11 +21,10 @@ export const checkWinner = (board = []) => {
         board[first] === board[second] &&
         board[second] === board[third]
       ) {
-        console.log(`Ganador ${board[first]}`)
         return board[first]
       }
     }
   }
 
-  return null
+  return false
 }
